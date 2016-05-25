89c5e5b67a156736975ab1173ce0cc07b3351754
Corinna Kuhn <corinnaerin@gmail.com>
2016-05-24 21:44:22 -0700

    + Update favicon + Minor theme modifications

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://inspectressgadget.corinnaerin.com',
+        url: 'http://www.inspectressgadget.com',
         mail: {},
         database: {
             client: 'postgres',
